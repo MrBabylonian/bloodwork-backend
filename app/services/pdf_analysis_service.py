7d24fc891bb178ebbaedaf7d8594bc54681bcb41
MrBabylonian <bedirhangilgiler@gmail.com>
2025-06-01 22:55:12 +0200

    VISION_SERVER_URL update

--- app/services/pdf_analysis_service.py
+++ app/services/pdf_analysis_service.py
@@ -15,7 +15,7 @@ logger = Logger.setup_logging().getChild("pdf_analysis_service")
 PDF_UPLOADS_ROOT_DIRECTORY: Path = Path("data/blood_work_pdfs")
 PDF_UPLOADS_ROOT_DIRECTORY.mkdir(parents = True, exist_ok = True)
 
-VISION_SERVER_URL = "http://localhost:4000"
+VISION_SERVER_URL = "http://13.53.174.62:4000"
 remote_vision_inference_service = RemoveVisionInferenceService(
 	VISION_SERVER_URL)
 
